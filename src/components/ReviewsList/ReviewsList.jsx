76d5afeec4751de46e0e5a8c1655cfd031722930
Domisiaxa <domisiaxa@gmail.com>
2023-04-13 00:47:58 +0200

    x

--- src/components/ReviewsList/ReviewsList.jsx
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -7,7 +7,7 @@ export const ReviewsList = ({ reviews, movieId }) => {
     <ul className={styles.list}>
       {reviews.map((review, index) => (
         <li className={styles.item} key={`${movieId}r${index + 1}`}>
-          <h4 className={styles.author}>{review.author}</h4>
+          <h4 className={styles.author}>Author: {review.author}</h4>
           <div className={styles.review}>
             <ParsedHtml toParse={review.content} />
           </div>
